054731adf8141c29e03f4909d846abe89e44f6c4
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-07 04:35:00 -0700

    video 17 - fetching data on component mount with useEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
-  return <div>App</div>;
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then((response) => {
+        console.log(response.data);
+        setResults(response.data.hits);
+      });
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {results.map((result) => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
